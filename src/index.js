5f0a7c577655b58d0a913425ce57944be9dcf0ee
noahpro <noahpro@gmail.com>
2025-08-05 13:03:46 -0400

    react router

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDom from 'react-dom';
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
-
-ReactDom.render(<App />, document.querySelector("#root"));
-
-// https://www.youtube.com/watch?v=G-Cr00UYokU
